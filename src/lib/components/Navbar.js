5cabbf4b177cb9750f3a269d8ae72d9b85e0f8e2
Kana Hashimoto <kana.hashimoto@mail.utoronto.ca>
2021-01-09 17:43:37 -0500

    Failed attempt to add a dark mode

--- src/lib/components/Navbar.js
+++ src/lib/components/Navbar.js
@@ -1,9 +1,11 @@
 import React from 'react';
-import { Button, Stack, Box, Center } from "@chakra-ui/react"
+import { Button, Stack, Box, Center, IconButton, useColorMode} from "@chakra-ui/react"
 
-class Navbar extends React.Component {
-  render() { 
-    let NavbarItems = (
+function Navbar() {
+  //For toggling dark mode
+  //const { colorMode, toggleColorMode } = useColorMode()
+
+  return ( 
       <Center>
       <Box borderRadius="lg" w="100%" h="100%" p={4}>
       <Stack direction="row" spacing={8} align="center">
@@ -12,7 +14,7 @@ class Navbar extends React.Component {
         </Box>
         <Button color="#ffffff" variant="ghost" _hover={{ bg: "#ffffff", color: "#22577A"}} 
         _focus={{ boxShadow: "0 0 1px 2px rgba(234, 180, 251, .75), 0 1px 1px rgba(0, 0, 0, .15)",
-        }}>
+      }}>
           D I A G N O S T I C S
         </Button>
         <Button color="#ffffff" variant="ghost" _hover={{ bg: "#ffffff", color: "#22577A"}} 
@@ -30,12 +32,16 @@ class Navbar extends React.Component {
       }}>
           A B O U T
         </Button>
+        {/* For toggling dark mode */}
+        {/* <IconButton 
+          icon={colorMode === 'light' ? 'moon' : 'sun'} 
+          onClick={toggleColorMode}
+          variant='ghost'
+          /> */}
       </Stack>
       </Box>
       </Center>
-    );
-    return NavbarItems;
-  }
+  );
 }
 
 export default Navbar;
---
+++ src/lib/theme.js
@@ -0,0 +1,38 @@
+// File used to define a default theme on the website
+// Defines button styling as well
+import { extendTheme } from "@chakra-ui/react"
+import { mode } from "@chakra-ui/theme-tools"
+
+const styles = {
+  global: props => ({
+    body: {
+      color: mode('blue.900, whiteAlpha.900')(props),
+      bg: mode('blue.50','blue.900')(props),
+    }
+  })
+};
+
+const components = {
+  Button: {
+    baseStyle: props => ({
+      color: mode('#ffffff','#ffffff')(props),
+      _hover: {
+        bg: mode('#ffffff','#22577A')(props), 
+        color: mode('#22577A','#ffffff')(props)
+      },
+      _focus: { 
+        boxShadow: "0 0 1px 2px rgba(234, 180, 251, .75), 0 1px 1px rgba(0, 0, 0, .15)",
+      }
+    }),
+    defaultProps: {
+      variant: 'ghost'
+    }
+  }
+};
+
+const globalTheme = extendTheme({
+  styles,
+  components
+});
+
+export default globalTheme
